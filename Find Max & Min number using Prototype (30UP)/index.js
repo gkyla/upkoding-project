9c0a76c5b3dbdeda6fa026055d9f857a133ea281
gkyla <agitsyang@gmail.com>
2021-06-01 02:48:41 +0700

    add comment

--- Find Max & Min number using Prototype (30UP)/index.js
+++ Find Max & Min number using Prototype (30UP)/index.js
@@ -1,6 +1,6 @@
 Number.prototype.maxNumber = function () {
   const split = this.toString().split('');
-  let max = +split[0];
+  let max = +split[0]; /* initial value */
   for (let i = 0; i < split.length; i++) {
     if (+split[i] > max) {
       max = split[i];
@@ -12,7 +12,7 @@ Number.prototype.maxNumber = function () {
 
 Number.prototype.minNumber = function () {
   const split = this.toString().split('');
-  let min = +split[0];
+  let min = +split[0]; /* initial value */
   for (let i = 0; i < split.length; i++) {
     if (+split[i] < min) {
       min = split[i];
